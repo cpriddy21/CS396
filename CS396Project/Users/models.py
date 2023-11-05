36b1f08c23bdcabddfa8ff80f54cfdfebb2cdcc4
cpriddy21 <cassandrapriddy21@gmail.com>
2023-11-04 20:25:06 -0500

    broken

--- CS396Project/Users/models.py
+++ CS396Project/Users/models.py
@@ -2,7 +2,7 @@ from django.apps import apps
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-from OnlineLearningSystem.models import Course
+#from OnlineLearningSystem.models import Course
 
 # Create your models here.
 
@@ -10,9 +10,9 @@ class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
     email = models.EmailField(unique=True)
-    def enrolled_courses(self):
-        Course = apps.get_model('OnlineLearningSystem', 'Course')  # Use apps.get_model to get the Course model
-        return Course.objects.filter(students=self)
+    # def enrolled_courses(self):
+    #     Course = apps.get_model('OnlineLearningSystem', 'Course')  # Use apps.get_model to get the Course model
+    #     return Course.objects.filter(students=self)
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='student')
